5dfea82d55338ed022f19b453ad36cda40e5eeee
Yannis <yannis.baltus@outlook.com>
2020-10-10 21:44:13 +0200

    add jms-serializer-bundle and init the api controller

---
+++ src/Controller/API/ProjectApiRestController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller\API;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/api/v1")
+ */
+class ProjectApiRestController extends AbstractController
+{
+}
